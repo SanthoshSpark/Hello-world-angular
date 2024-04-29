3bc0b500f1b77fa850421274b7880156b4afa8e0
SanthoshSpark <santhoshdurairaj001@gmail.com>
2024-04-29 19:41:10 +0530

    first commit

---
+++ src/app/hw.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector :'app-hw',
+    templateUrl:'./hw.component.html',
+    styleUrls:['./hw.component.css']
+})
+
+export class Helloworld{
+
+    title = "Hello World";
+}
